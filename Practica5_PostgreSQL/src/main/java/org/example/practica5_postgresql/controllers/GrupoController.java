b48b7e5bf5ac86a8f385ff2a056b54ce683a6dd4
efernandezcostas <efernandezcostas@danielcastelao.org>
2025-03-13 21:27:28 +0100

    añadidos metodos

--- Practica5_PostgreSQL/src/main/java/org/example/practica5_postgresql/controllers/GrupoController.java
+++ Practica5_PostgreSQL/src/main/java/org/example/practica5_postgresql/controllers/GrupoController.java
@@ -3,17 +3,22 @@ package org.example.practica5_postgresql.controllers;
 import org.example.practica5_postgresql.models.Grupo;
 import org.example.practica5_postgresql.repositories.GrupoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/grupos")
 public class GrupoController {
     private final GrupoRepository grupoRepository;
+    private final RestTemplate restTemplate;
 
     @Autowired
     public GrupoController(GrupoRepository grupoRepository){
         this.grupoRepository = grupoRepository;
+        this.restTemplate = new RestTemplate();
     }
 
     // Solo en PostgreSQL
@@ -39,16 +44,40 @@ public class GrupoController {
         return ResponseEntity.ok("Grupo eliminado.");
     }
 
+    @DeleteMapping("/delete/todos")
+    public ResponseEntity<String> delete(){
+        grupoRepository.deleteAll();
+        return ResponseEntity.ok("Grupos eliminados");
+    }
+
     // PostgreSQL y MongoDB
     @PostMapping("/crear")
-    public void crear(){
+    public ResponseEntity<String> crear(@RequestBody Grupo grupo){
+        String respuesta = "";
 
+        ResponseEntity<String> respuestaPostgres = add(grupo);
+        if (respuestaPostgres.getStatusCode() != HttpStatus.OK){
+            respuesta += "El grupo no ha sido añadido a PostgreSQL: "+respuestaPostgres.getBody()+"\n";
+        }
+        try {
+            restTemplate.postForEntity("http://localhost:8080/grupos/crear", grupo, String.class);
+        } catch (HttpClientErrorException e){
+            respuesta += "El grupo no ha sido añadido a MongoDB: "+e.getResponseBodyAsString();
+        }
+
+        if (!respuesta.isEmpty()){
+            return ResponseEntity.badRequest().body(respuesta);
+        }
+
+        return ResponseEntity.ok("Grupo añadido en PostgreSQL y MongoDB");
     }
 
     // PostgreSQL y MongoDB
     @DeleteMapping("/borrar/{id}")
-    public void borrar(@PathVariable String id){
-
+    public ResponseEntity<String> borrar(@PathVariable String id){
+        delete(id);
+        restTemplate.delete("http://localhost:8080/grupos/borrar/"+id);
+        return ResponseEntity.ok("Grupo eliinado en PostgreSQL y MongoDB");
     }
 
     private Boolean comprobarCampos(String id, String nombre){
